cf4041b348e4ba00928f5d1671853f03fa69354c
Muhammad Zuhairi <47267077+mzuhairiy@users.noreply.github.com>
2026-02-13 22:08:27 +0700

    feat: implement product addition and user data retrieval APIs, add .env.example

---
+++ app/api/user/data/route.js
@@ -0,0 +1,23 @@
+import dbConnect from "@/config/db";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request);
+
+        await dbConnect()
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({ success: true, user }, { status: 200 });
+        
+    } catch (error) {
+        console.log("Error fetching user data:", error);
+        return NextResponse.json({ success: false, message: "Internal Server Error" }, { status: 500 });
+    }
+}
